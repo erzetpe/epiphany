3a66404fe928be3d7bdc375433e6fd1bf4918bc7
erzetpe <erzetpe@gmail.com>
2019-12-05 12:07:30 +0100

    Move part of helpers to core module, change code for snapshots

--- core/src/backup/create.py
+++ core/src/backup/create.py
@@ -1,5 +1,7 @@
 import backup.cloud.azure.create_snapshots as azure_create_snapshots
 from core.helpers.data_loader import load_yaml_file
+from core.helpers.doc_list_helpers import select_single
+
 
 def init(sub_parser):
     sub_parser = sub_parser.add_parser('create', description='Create disk snapshots')
@@ -8,28 +10,38 @@ def init(sub_parser):
     sub_parser.set_defaults(func=run)
 
 
+def run_azure(cluster_config):
+    credentials = cluster_config["specification"]["cloud"]["credentials"]
+    client_id = credentials["client_id"]
+    client_secret = credentials["client_secret"]
+    subscription_id = cluster_config["specification"]["cloud"]["subscription_name"]
+    tenant_id = credentials["tenant_id"]
+    resource_group_name = f"{cluster_config['specification']['prefix']}-{cluster_config['specification']['name']}-rg"
+
+    azure_create_snapshots.create_disks_snapshots(client_id=client_id, client_secret=client_secret,
+                                                  tenant_id=tenant_id, subscription_id=subscription_id,
+                                                  resource_group_name=resource_group_name)
+
+
 def run(args):
 
     if "file" in args and args.file:
         docs = load_yaml_file(args.file)
+        cluster_config = select_single(docs, lambda x: x["kind"] == 'epiphany-cluster')
 
-        for doc in docs:
-            print(doc)
+        if cluster_config["provider"] == "azure":
 
-    else:
-        print("Woooha...")
-        exit(1)
+            if args.disks_snapshot:
+                run_azure(cluster_config)
 
-    client = ""
-    key = ""
-    subscription_id = ""
-    tenant_id = ""
-    resource_group_name = ""
+            else:
+                print("Woooha... No parameter?")
+                exit(1)
 
-    if args.disks_snapshot:
-        azure_create_snapshots.create_disks_snapshots(client_id=client, client_secret=key,
-                                              tenant_id=tenant_id, subscription_id=subscription_id,
-                                              resource_group_name=resource_group_name)
+        else:
+            print("Woooha... Not implemented yet.")
+            exit(1)
+
     else:
         print("Woooha...")
         exit(1)
